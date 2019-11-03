2b98edadcca0ac51316f9e1c916b1cffc73decc4
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-11-03 15:38:17 +0530

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,8 @@
 
 namespace App\Providers;
 
-use Storage;
 use Laravel\Passport\Passport;
-use League\Flysystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
-use Spatie\Dropbox\Client as DropboxClient;
-use Spatie\FlysystemDropbox\DropboxAdapter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,12 +20,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Storage::extend('dropbox', function ($app, $config) {
-            $client = new DropboxClient(
-                $config['authorizationToken']
-            );
-
-            return new Filesystem(new DropboxAdapter($client));
-        });
     }
 }
